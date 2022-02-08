e70f54ec999c16e9986a300be482edbe1ad92b37
kaakeli <60170508+kaakeli@users.noreply.github.com>
2022-02-08 20:24:54 +0200

    Add project files.

---
+++ TheKello/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace TheKello
+{
+    partial class TheKello
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TheKello));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Color = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("BatmanForeverAlternate", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kello";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.HelpRequest += new System.EventHandler(this.Label1_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.AccessibleDescription = "exit";
+            this.Exit.AccessibleName = "exit";
+            this.Exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.Exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Exit.Image = ((System.Drawing.Image)(resources.GetObject("Exit.Image")));
+            this.Exit.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Exit.Location = new System.Drawing.Point(4, -2);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(30, 20);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "X";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Color
+            // 
+            this.Color.AccessibleName = "text color";
+            this.Color.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.Color.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Color.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Color.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Color.Image = ((System.Drawing.Image)(resources.GetObject("Color.Image")));
+            this.Color.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.Color.Location = new System.Drawing.Point(40, -2);
+            this.Color.Name = "Color";
+            this.Color.Size = new System.Drawing.Size(27, 20);
+            this.Color.TabIndex = 2;
+            this.Color.Tag = "text color";
+            this.Color.Text = "T";
+            this.Color.UseVisualStyleBackColor = true;
+            this.Color.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Apply += new System.EventHandler(this.Label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleDescription = "trancperancy";
+            this.button1.AccessibleName = "trancperancy";
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(111, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "TP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "back ground color";
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.Location = new System.Drawing.Point(73, -2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 20);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "BGC";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TheKello
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(250, 117);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Color);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TheKello";
+            this.ShowInTaskbar = false;
+            this.Text = "TheKello";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Color;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TheKello/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheKello
+{
+    public partial class TheKello : Form
+    {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+        
+
+        public TheKello()
+        {
+            InitializeComponent();
+            
+            //this.TransparencyKey = (BackColor);
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("HH:mm");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            //if (Properties.Settings.Default.F1Size.Width == 0) Properties.Settings.Default.Upgrade();
+
+            //if (Properties.Settings.Default.F1Size.Width == 0 || Properties.Settings.Default.F1Size.Height == 0)
+            //{
+            //    // first start
+            //    // optional: add default values
+            //}
+            //else
+            //{
+            //    this.WindowState = Properties.Settings.Default.F1State;
+
+            //    // we don't want a minimized window at startup
+            //    if (this.WindowState == FormWindowState.Minimized) this.WindowState = FormWindowState.Normal;
+
+            //    this.Location = Properties.Settings.Default.F1Location;
+            //    this.Size = Properties.Settings.Default.F1Size;
+            //}
+        }
+        // FORM MOVE
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        // LABEL BUTTON
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            //this.TransparencyKey = (BackColor);
+
+            DialogResult fontResult = fontDialog1.ShowDialog();
+            if (fontResult == DialogResult.OK)
+            {
+                label1.Font = fontDialog1.Font;
+            }
+
+        }
+
+        // EXIT BUTTON
+        private void Button1_Click(object sender, EventArgs e)
+        {            
+            Application.Exit();
+        }
+
+        // COLOR
+        private void Color_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            label1.ForeColor = colorDialog1.Color;
+        }
+
+        // SAVE APPSETTINGS
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            //Properties.Settings.Default.F1State = this.WindowState;
+            //if (this.WindowState == FormWindowState.Normal)
+            //{
+            //    // save location and size if the state is normal
+            //    Properties.Settings.Default.F1Location = this.Location;
+            //    Properties.Settings.Default.F1Size = this.Size;
+            //}
+            //else
+            //{
+            //    // save the RestoreBounds if the form is minimized or maximized!
+            //    Properties.Settings.Default.F1Location = this.RestoreBounds.Location;
+            //    Properties.Settings.Default.F1Size = this.RestoreBounds.Size;
+            //}
+
+            //// don't forget to save the settings
+            //Properties.Settings.Default.Save();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.TransparencyKey = (BackColor);
+        }        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog2.ShowDialog();
+            label1.BackColor = colorDialog2.Color;
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+    
+}
